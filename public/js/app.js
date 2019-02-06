75711616d7777dd372420ee3edd6d8dcc581165c
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2019-02-06 19:18:59 +0200

    lesson 20. Маска при добавлении в корзину

--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,18 @@ cartInHeader = $('#cart-in-header');
 
 addToCartButtons.on('click', function(event) {
     event.preventDefault();
-
-    console.log('Кнопка нажата.');
+    mask();
 
     $.get(event.target.href, function (data) {
         cartInHeader.html(data);
+        unmask();
     });
 });
 
+function mask() {
+    $('body').append('<div class="lmask"></div>');
+}
+
+function unmask() {
+    $('.lmask').remove();
+}
