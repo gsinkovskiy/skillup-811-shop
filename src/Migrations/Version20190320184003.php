432f7bb5f411c56af46aadb8df8a5e5c0a8a806f
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2019-03-20 21:28:40 +0200

    lesson 30. Деревья

---
+++ src/Migrations/Version20190320184003.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190320184003 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE categories ADD lft INT NOT NULL DEFAULT 0, ADD lvl INT NOT NULL DEFAULT 0, ADD rgt INT NOT NULL DEFAULT 0');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE categories DROP lft, DROP lvl, DROP rgt');
+    }
+}
